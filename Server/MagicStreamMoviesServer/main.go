efcfcbe6528b7a9f803a0f87bd590e1b2848ec87
Shawnq-create <enxiang326@gmail.com>
2025-10-20 17:33:32 +0200

    initial project commit

---
+++ Server/MagicStreamMoviesServer/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	controller "github.com/Shawnq-create/MagicStreamMovies/server/MagicStreamMoviesServer/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	router.GET("/movies", controller.GetMovies())
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Shawnq-create/MagicStreamMovies/server/MagicStreamMoviesServer
+
+go 1.25.3
